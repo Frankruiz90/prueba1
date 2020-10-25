50ea5a2820ebef956b37b27625394e6a3fc34fe9
unknown <frankr@neumomed.com>
2020-10-25 13:26:05 -0500

    feature/ nex navbar add functionality and animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="home">
-    <new-header />
-    <history />
-    <my-form />
+    <my-nav-bar />
+    <new-header id="new-header" />
+    <history id="history" />
+    <my-form id="my-form" />
     <my-footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import NewHeader from '@/components/NewHeader.vue'
-import History from '@/components/History.vue'
-import MyForm from '@/components/MyForm.vue'
-import MyFooter from '@/components/MyFooter.vue'
+import NewHeader from "@/components/NewHeader.vue";
+import History from "@/components/History.vue";
+import MyForm from "@/components/MyForm.vue";
+import MyFooter from "@/components/MyFooter.vue";
+import MyNavBar from "@/components/MyNavBar.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     NewHeader,
     History,
     MyForm,
-    MyFooter
-  }
-}
+    MyFooter,
+    MyNavBar,
+  },
+};
 </script>
